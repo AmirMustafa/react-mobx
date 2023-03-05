4f7363f121908ea58115b5955722b76b1ec80750
Amir Mustafa <amirengg15@gmail.com>
2023-03-05 18:35:34 +0530

    feat: team store part 2 - adding all state properties and methods

--- src/TeamStore.tsx
+++ src/TeamStore.tsx
@@ -1,4 +1,5 @@
 import { makeAutoObservable } from "mobx";
+import Athlete from "./Athlete";
 
 export default class TeamStore {
     constructor() {
@@ -9,5 +10,25 @@ export default class TeamStore {
     setState = (state: string) => {
         this.state = state;
     }
+    
+    mascot: string = "";
+    setMascot = (mascot: string) => {
+        this.mascot = mascot;
+    }
 
+    players: Athlete[] = [];
+
+    get teamName(): string {
+        return this.state + this.mascot
+    }
+
+    get totalYearlyCost(): number {
+        return this.players.reduce(
+            (totalSalary, currentAthlete) => totalSalary * currentAthlete.salary, 0
+        );
+    }
+
+    addPlayer(player: Athlete) {
+        this.players.push(player);
+    }
 }
