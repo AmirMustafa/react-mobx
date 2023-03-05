23188f36b9441e60df704197b902f2123b1f3de5
Amir Mustafa <amirengg15@gmail.com>
2023-03-05 17:36:11 +0530

    feat: convert React state to MobX state

--- src/MoneyForm.tsx
+++ src/MoneyForm.tsx
@@ -1,28 +1,46 @@
-import React, {useState} from 'react';
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+type FormState = {
+    total: number;
+    years: number;
+    salary: number;
+};
+
+const formState: FormState = observable({
+    total: 0,
+    years: 0,
+    salary: 0,
+});
 
 const MoneyForm = () => {
-    const [total, setTotal] = useState<number>(0);
-    const [years, setYears] = useState(0);
-    const [salary, setSalary] = useState(0);
+    const calculateTotal = action((formState: FormState) => {
+        formState.total = formState.salary * formState.years;
+        console.log('formState.total ===> ', formState.salary, formState.years, formState.total);
+    })
 
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
             <h1 style={{ marginBottom: 0 }}>Money Talks</h1>
-            <p>Total: {total}</p>
+            <p>Total: { formState.total }</p>
             <input 
                 type='number'  
                 placeholder="Years..."
                 style={{ height: '40px' }}
-                onChange={(e) => setYears(+e.target.value)}
+                onChange={action((e) => {
+                    formState.years = +e.target.value
+                } )}
             />
             <input 
                 type='number'  
                 placeholder="Yearly Salary..."
                 style={{ height: '40px' }}
-                onChange={(e) => setSalary(+e.target.value)}
+                onChange={action((e) => {
+                    formState.salary = +e.target.value
+                } )}
             />
-            <button type="button" onClick={() => setTotal(years * salary)}>Calculate Total</button>
+            <button type="button" onClick={() => calculateTotal(formState)}>Calculate Total</button>
         </div>
     );
 }
-export default MoneyForm;
+export default observer(MoneyForm);
