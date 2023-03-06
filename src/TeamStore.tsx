c9164d57a9d3c6a016c4e5a8747aad2f165ad860
Amir Mustafa <amirengg15@gmail.com>
2023-03-06 11:11:26 +0530

    feat: adding TeamStoreContext in team store

--- src/TeamStore.tsx
+++ src/TeamStore.tsx
@@ -1,8 +1,10 @@
 import { makeAutoObservable } from "mobx";
+import React, { useContext, useRef } from "react";
 import Athlete from "./Athlete";
 
 export default class TeamStore {
-    constructor() {
+    constructor(players: Athlete[]) {
+        this.players = players;
         makeAutoObservable(this);
     }
 
@@ -32,3 +34,22 @@ export default class TeamStore {
         this.players.push(player);
     }
 }
+
+const TeamStoreContext = React.createContext<TeamStore>(null as unknown as TeamStore);
+
+export const useTeamStore = () => useContext(TeamStoreContext);
+
+type Props = {
+    children: React.ReactNode,
+    players: Athlete[]
+};
+
+export function TeamStoreProvider({children, players}: Props) {
+    const store = useRef(new TeamStore(players));
+    return (
+        <TeamStoreContext.Provider value={store.current}>
+            {children}
+        </TeamStoreContext.Provider>
+    );
+}
+
